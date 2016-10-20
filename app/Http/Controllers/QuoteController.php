eeac5069160f29045ebb27a3f2df565eedb35ba6
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-10-20 23:02:34 +0000

    implement pagination

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -13,10 +13,10 @@ class QuoteController extends Controller
         if (!is_null($author)) {
             $quote_author = Author::where('name', $author)->first();
             if ($quote_author) {
-                $quotes = $quote_author->quotes()->orderBy('created_at', 'desc')->get();
+                $quotes = $quote_author->quotes()->orderBy('created_at', 'desc')->paginate(6);
             }
         } else {
-            $quotes = Quote::orderBy('created_at', 'desc')->get();
+            $quotes = Quote::orderBy('created_at', 'desc')->paginate(6);
         }
         return view('index', ['quotes' => $quotes]);
     }
