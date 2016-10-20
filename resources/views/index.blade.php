22ae27ac262826b913894a99203b374605573d93
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-10-20 22:54:21 +0000

    add filtering by author name via routes

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,6 +9,11 @@
 @endsection
 
 @section('content')
+    @if(!empty(Request::segment(1)))
+        <section class="filter-bar">
+            A filter has been set <a href="{{ route('index') }}">Show all quotes</a>
+        </section>
+    @endif
     @if(count($errors) > 0)
         <section class="info-box fail">
             @foreach($errors->all() as $error)
@@ -27,7 +32,7 @@
             <article class="quote">
             <div class="delete"><a href="{{ route('delete', ['quote_id' => $quotes[$i]->id]) }}">x</a></div>
             {{ $quotes[$i]->content }}
-            <div class="info">Created by <a href="#">{{ $quotes[$i]->author->name }}</a> on {{ $quotes[$i]->created_at }}</div>
+            <div class="info">Created by <a href="{{ route('index', ['author' => $quotes[$i]->author->name]) }}">{{ $quotes[$i]->author->name }}</a> on {{ $quotes[$i]->created_at }}</div>
         </article>
         @endfor
         <div class="pagination">
