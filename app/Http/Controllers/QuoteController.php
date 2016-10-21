bc697ffa53714ec93a065bb8ef340eda79039265
Sebastian Adam Prokuski <sprokuski@gmail.com>
2016-10-21 18:40:18 +0000

    add event for logging quote author in separate table

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Author;
 use App\Quote;
 use Illuminate\Http\Request;
+use App\Events\QuoteCreated;
+use Illuminate\Support\Facades\Event;
 
 class QuoteController extends Controller
 {
@@ -40,6 +42,8 @@ class QuoteController extends Controller
         $quote = new Quote();
         $quote->content = $quoteText;
         $author->quotes()->save($quote);
+        
+        Event::fire(new QuoteCreated($author));
         
         return redirect()->route('index')->with([
             'success' => 'Quote saved!'    
